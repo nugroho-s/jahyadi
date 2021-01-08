9f13bdcd67d401eb6ec21f59ddf1031a067c12e0
Nugroho Satriyanto <nugroho.satriyanto@traveloka.com>
2021-01-08 23:07:30 +0700

    check other's balance

--- command/economy/Balance.py
+++ command/economy/Balance.py
@@ -1,4 +1,7 @@
+import re
 from datetime import datetime
+
+import discord
 
 from command.economy.EconomyBase import EconomyBase
 from model.User import User
@@ -14,6 +17,15 @@ class Balance(EconomyBase):
 
     async def do_response(self, message, args):
         await super().do_response(message, args)
-        user = self.session.query(User).filter(User.user_id == message.author.id).one()
-        userDiscord = await self.client.fetch_user(user.user_id)
-        await try_send(message.channel, 'jahyadi coin {} {}'.format(userDiscord.name, user.jahyadi_coin))
+        user_id = message.author.id
+        if len(args) > 2:
+            user_id = args[2]
+            user_id = re.findall("\d+", user_id)[0]
+            try:
+                user = await self.client.fetch_user(int(user_id))
+            except discord.errors.NotFound:
+                self.logging.error("User not found: {}".format(user_id))
+                return
+        user = self.session.query(User).filter(User.user_id == user_id).one()
+        user_discord = await self.client.fetch_user(user.user_id)
+        await try_send(message.channel, 'jahyadi coin {} {}'.format(user_discord.name, user.jahyadi_coin))
