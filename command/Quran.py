cc752f35befbfd6dffbbfd23013395b1ebdb3942
Nugroho Satriyanto <nugroho.satriyanto@traveloka.com>
2021-04-05 18:59:49 +0700

    except file not found

--- command/Quran.py
+++ command/Quran.py
@@ -16,20 +16,23 @@ class Quran(Command):
         if len(args) < 3:
             return
         url = "resources/quran/{}.json".format(args[2])
-        with open(url) as quran_response:
-            data = json.load(quran_response)
-            surah = data[next(iter(data))]
-            if len(args) == 3:
-                response = discord.Embed(title=surah['name_latin'])
-                response.add_field(name='arabic', value=surah['name'], inline=False)
-                response.add_field(name='jumlah ayat', value=surah['number_of_ayah'], inline=False)
-                response.add_field(name='arti', value=surah['translations']['id']['name'], inline=False)
-            if len(args) == 4:
-                response = discord.Embed(title="{}:{}".format(surah['name_latin'], args[3]))
-                response.add_field(name='arabic', value=surah['text'][args[3]][:1024], inline=False)
-                translations_text = surah['translations']['id']['text'][args[3]]
-                if len(translations_text) > 1024:
-                    translations_text = translations_text[:1021] + '...'
-                response.add_field(name='arti', value=translations_text, inline=False)
-                self.logging.info(surah['translations']['id']['text'][args[3]])
-            await try_send(message.channel, embed=response)
+        try:
+            with open(url) as quran_response:
+                data = json.load(quran_response)
+                surah = data[next(iter(data))]
+                if len(args) == 3:
+                    response = discord.Embed(title=surah['name_latin'])
+                    response.add_field(name='arabic', value=surah['name'], inline=False)
+                    response.add_field(name='jumlah ayat', value=surah['number_of_ayah'], inline=False)
+                    response.add_field(name='arti', value=surah['translations']['id']['name'], inline=False)
+                if len(args) == 4:
+                    response = discord.Embed(title="{}:{}".format(surah['name_latin'], args[3]))
+                    response.add_field(name='arabic', value=surah['text'][args[3]][:1024], inline=False)
+                    translations_text = surah['translations']['id']['text'][args[3]]
+                    if len(translations_text) > 1024:
+                        translations_text = translations_text[:1021] + '...'
+                    response.add_field(name='arti', value=translations_text, inline=False)
+                    self.logging.info(surah['translations']['id']['text'][args[3]])
+                await try_send(message.channel, embed=response)
+        except FileNotFoundError as err:
+            self.logging.error('Surah {} not found'.format(':'.join(args[2:])))
